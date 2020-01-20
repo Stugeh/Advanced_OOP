61f9fd97cc9e9127380652870c19304794c32d21
Stugeh <tuukkaveteli@gmail.com>
2020-01-20 17:05:25 +0200

    working version

--- vt1/isSqrMatrix.cpp
+++ vt1/isSqrMatrix.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool bracketLoop(stringstream &input, int &bracketCount) {
+bool bracketLoop(stringstream &input, int &bracketCount, int& numCount, int& prevCount, int& openBracket) {
     bool loop = true;
     int num;
     char c;
@@ -12,34 +12,65 @@ bool bracketLoop(stringstream &input, int &bracketCount) {
     if( !input.good() || c!='['){
         return false;
     }
+    openBracket += 1;
     bracketCount += 1;
     for(;;){
         input >> num;
         if (!input.good()) {
             return false;
         }
+        numCount += 1;
 
         input >> c;
         if (input.good() && c == ']') {
             bracketCount -= 1;
-            return true;
+            if (prevCount != numCount && prevCount != 0 ){
+                return false;
+            }
+            prevCount = numCount;
+            numCount=0;
+            input >> c;
+            if (input.good() && c==']'){
+                bracketCount -= 1;
+                input >> c;
+                if (!input.good()){
+                    return true;
+                }
+            }else if(c == '['){
+                openBracket += 1;
+            }
+            else if (!input.good()){
+                input >> num;
+                if(!input.good()){
+                    return false;
+                }
+            }
         }
     }
 }
 
 bool isSqrMatrix(const string& matrix){
+    /*
+     * bracketCount keeps track that all brackets close.
+     * numCount is used to check if the current bracket has an equal amount of values to the previous one.
+     * prevCount is the amount of values in the previous brackets.
+     * openBrackets is used to check if the number of bracket is the same as the number of values in each bracket.
+     */
     stringstream input(matrix);
     char c;
-    int num=0, bracketCount=0;
+    int num=0, bracketCount=0, numCount=0, prevCount=0, openBracket=0;
 
     input >> c;
     if(!input.good() || c != '['){
        return false;
     }
     bracketCount += 1;
-    bool isSq;
-    while (bracketCount<0 && isSq){
-        isSq = bracketLoop(input, bracketCount);
+    bool isSq = true;
+    while (bracketCount>0 && isSq){
+        isSq = bracketLoop(input, bracketCount, numCount, prevCount, openBracket);
+    }
+    if (openBracket != prevCount){
+        isSq = false;
     }
     return isSq;
 }
--- vt1/main.cpp
+++ vt1/main.cpp
@@ -1,12 +1,34 @@
+#define CATCH_CONFIG_MAIN
 #include <iostream>
 #include "catch.hpp"
 #include "isSqrMatrix.h"
 using namespace std;
 
+TEST_CASE("test 1","[correct]"){
+    CHECK(isSqrMatrix("[[2,3][1,2]]"));
+    CHECK(isSqrMatrix("[[2,3,1][1,2,45][3,4,7]]"));
+    CHECK(isSqrMatrix("[[2]]"));
+    CHECK(isSqrMatrix("[[2,3,1,2][1,2,45,125][3,4,7,1256][124,125,125,125]]"));
+}
+TEST_CASE("test 2","[incorrect]"){
+    CHECK_FALSE(isSqrMatrix("[[2,3]]"));
+    CHECK_FALSE(isSqrMatrix("[2,3][1,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[,3][1,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[23]1,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[2,3[1,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[2,3][1,2]"));
+    CHECK_FALSE(isSqrMatrix("[[2,,3][1,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[2,3][12]]"));
+    CHECK_FALSE(isSqrMatrix("[[2,3,1][1,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[2,3][1,2,2]]"));
+    CHECK_FALSE(isSqrMatrix("[[2,3][1,2][1,3]"));
+}
+
+/*
 int main() {
-    string matrix;
-    cout << "Square matrix: " << endl;
-    cin >> matrix;
+    string matrix="[[1,2][3,4][5,6]]";
+    //cout << "Square matrix: " << endl;
+    //cin >> matrix;
     if (isSqrMatrix(matrix)){
         cout<< "It's a square matrix" << endl;
     }
@@ -15,3 +37,4 @@ int main() {
     }
     return 0;
 }
+*/
